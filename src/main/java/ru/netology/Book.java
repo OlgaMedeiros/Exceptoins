202d43953b8e32c21e6d1a1943e7739db6e62ab1
OlgaMed <mama2482@mail.ru>
2023-04-04 22:03:38 +0400

    project preparationÐ

---
+++ src/main/java/ru/netology/Book.java
@@ -0,0 +1,19 @@
+
+package ru.netology;
+public class Book extends Exceptions {          //book - дочерний класс ; Product - родительский класс
+    private String name;
+    private String author;
+
+    public Book(String name, String author, int id, int price) {   // все поля для Book
+        super(id, name, price);  // все общие поля для книги и смартфона
+        this.author = author;    // поле только для класса Book
+    }
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+}
---
+++ src/main/java/ru/netology/Repository.java
@@ -0,0 +1,49 @@
+package ru.netology;
+
+public class Repository {
+    private Product[] products = new Product[0];  //изначально в массиве Product 0 ячеек
+
+    public Product[] findAll() {
+        return products;
+    }
+
+    public void save(Product product) {         // сохраняем продукты
+        Product[] tmp = new Product[products.length + 1];   //временный массив с длиной на одну ячейку больше, чем в Product
+        for (int i = 0; i < products.length; i++) {   //цикл можно создать с помощью products.fori и нажать tab
+            tmp[i] = products[i];  //присваиваем ячейке tmp product
+
+        }
+        tmp[tmp.length - 1] = product;
+        products = tmp;
+    }
+
+    public void removeById(int id) {  //удаляем продукт по Id
+
+        Product[] tmp = new Product[products.length - 1];
+        int copyToIndex = 0;
+        for (Product product : products) {
+            if (product.getId() != id) {
+                tmp[copyToIndex] = product;
+                copyToIndex++;
+
+
+            }
+        }
+        products = tmp;
+    }
+
+
+    public Product[] findById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return new Product[]{product};
+            }
+        }
+        return null;
+    }
+
+    public Product[] products() {
+        return products;
+    }
+}
+}
